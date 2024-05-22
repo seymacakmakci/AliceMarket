db1d928e0e68b5f6c7c82c05a5baaa498751741b
Seyma <sseymacakmakci@gmail.com>
2024-05-22 16:05:16 +0300

    [Feat] Models folder created

---
+++ AliceMarket/AliceMarket/Models/ProductsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliceMarket.Models
+{
+    public class ProductsModel
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
