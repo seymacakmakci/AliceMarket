6599406e48933a86ef191cfbffcc71adb0d46228
Seyma <sseymacakmakci@gmail.com>
2024-05-22 16:37:45 +0300

    [Add] CategoriesModel and OrdersModel created

---
+++ AliceMarket/AliceMarket/Contexts/AliceMarketDbContext.cs
@@ -0,0 +1,19 @@
+﻿using AliceMarket.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliceMarket.Contexts
+{
+    internal class AliceMarketDbContext
+    {
+        public DbSet<CustomersModel> Customers { get; set; }
+        public DbSet<ProductsModel> Products { get; set; }
+        public DbSet<OrdersModel> Orders { get; set; }
+        public DbSet<CategoriesModel> Categories { get; set; }
+
+    }
+}
---
+++ AliceMarket/AliceMarket/Models/OrdersModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliceMarket.Models
+{
+    public class OrdersModel
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public int ProductId { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
