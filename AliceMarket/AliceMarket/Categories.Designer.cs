0e212b0565cbbd7fb080f2dbaa778676f49dd87d
Seyma <sseymacakmakci@gmail.com>
2024-05-24 15:40:18 +0300

    [Add] Categories form with CRUD operations
    - Created Categories form with functionalities to list, add, edit, and delete categories.
    - Implemented methods to handle CRUD operations.
    - Added event handlers for buttons and DataGridView cell clicks.
    - Included search functionality to filter categories by name.

--- AliceMarket/AliceMarket/Categories.Designer.cs
+++ AliceMarket/AliceMarket/Categories.Designer.cs
@@ -57,8 +57,9 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1164, 98);
+            this.panel1.Size = new System.Drawing.Size(776, 64);
             this.panel1.TabIndex = 3;
             // 
             // label2
@@ -66,9 +67,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Verdana", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FloralWhite;
-            this.label2.Location = new System.Drawing.Point(477, 9);
+            this.label2.Location = new System.Drawing.Point(318, 6);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(241, 38);
+            this.label2.Size = new System.Drawing.Size(164, 26);
             this.label2.TabIndex = 2;
             this.label2.Text = "Alice\'s Market";
             // 
@@ -77,9 +79,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Verdana", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FloralWhite;
-            this.label1.Location = new System.Drawing.Point(512, 47);
+            this.label1.Location = new System.Drawing.Point(341, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(167, 34);
+            this.label1.Size = new System.Drawing.Size(111, 23);
             this.label1.TabIndex = 0;
             this.label1.Text = "Categories";
             // 
@@ -87,9 +90,10 @@
             // 
             this.panel2.BackColor = System.Drawing.Color.LimeGreen;
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 731);
+            this.panel2.Location = new System.Drawing.Point(0, 476);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1164, 13);
+            this.panel2.Size = new System.Drawing.Size(776, 8);
             this.panel2.TabIndex = 4;
             // 
             // dgvCategories
@@ -107,20 +111,22 @@
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.dgvCategories.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dgvCategories.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCategories.Location = new System.Drawing.Point(30, 213);
+            this.dgvCategories.Location = new System.Drawing.Point(20, 138);
+            this.dgvCategories.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgvCategories.Name = "dgvCategories";
             this.dgvCategories.RowHeadersWidth = 62;
             this.dgvCategories.RowTemplate.Height = 28;
-            this.dgvCategories.Size = new System.Drawing.Size(573, 453);
+            this.dgvCategories.Size = new System.Drawing.Size(382, 294);
             this.dgvCategories.TabIndex = 17;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(670, 223);
+            this.label4.Location = new System.Drawing.Point(447, 145);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(32, 22);
+            this.label4.Size = new System.Drawing.Size(21, 14);
             this.label4.TabIndex = 18;
             this.label4.Text = "ID";
             // 
@@ -128,9 +134,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(629, 329);
+            this.label3.Location = new System.Drawing.Point(419, 214);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(152, 22);
+            this.label3.Size = new System.Drawing.Size(105, 14);
             this.label3.TabIndex = 19;
             this.label3.Text = "Category Name";
             // 
@@ -138,34 +145,38 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(670, 456);
+            this.label5.Location = new System.Drawing.Point(447, 296);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(111, 22);
+            this.label5.Size = new System.Drawing.Size(77, 14);
             this.label5.TabIndex = 20;
             this.label5.Text = "Description";
             // 
             // txtCategoryID
             // 
             this.txtCategoryID.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.txtCategoryID.Location = new System.Drawing.Point(802, 223);
+            this.txtCategoryID.Location = new System.Drawing.Point(535, 145);
+            this.txtCategoryID.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtCategoryID.Name = "txtCategoryID";
-            this.txtCategoryID.Size = new System.Drawing.Size(282, 26);
+            this.txtCategoryID.Size = new System.Drawing.Size(189, 20);
             this.txtCategoryID.TabIndex = 24;
             // 
             // txtCtgName
             // 
             this.txtCtgName.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.txtCtgName.Location = new System.Drawing.Point(802, 329);
+            this.txtCtgName.Location = new System.Drawing.Point(535, 214);
+            this.txtCtgName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtCtgName.Name = "txtCtgName";
-            this.txtCtgName.Size = new System.Drawing.Size(282, 26);
+            this.txtCtgName.Size = new System.Drawing.Size(189, 20);
             this.txtCtgName.TabIndex = 25;
             // 
             // tctCtgDesc
             // 
             this.tctCtgDesc.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.tctCtgDesc.Location = new System.Drawing.Point(803, 455);
+            this.tctCtgDesc.Location = new System.Drawing.Point(535, 296);
+            this.tctCtgDesc.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tctCtgDesc.Name = "tctCtgDesc";
-            this.tctCtgDesc.Size = new System.Drawing.Size(282, 26);
+            this.tctCtgDesc.Size = new System.Drawing.Size(189, 20);
             this.tctCtgDesc.TabIndex = 26;
             // 
             // btnAdd
@@ -175,9 +186,10 @@
             this.btnAdd.FlatAppearance.BorderSize = 0;
             this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAdd.ForeColor = System.Drawing.Color.FloralWhite;
-            this.btnAdd.Location = new System.Drawing.Point(674, 582);
+            this.btnAdd.Location = new System.Drawing.Point(449, 378);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(96, 45);
+            this.btnAdd.Size = new System.Drawing.Size(64, 29);
             this.btnAdd.TabIndex = 29;
             this.btnAdd.Text = "ADD";
             this.btnAdd.UseVisualStyleBackColor = false;
@@ -189,9 +201,10 @@
             this.btnEdit.FlatAppearance.BorderSize = 0;
             this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnEdit.ForeColor = System.Drawing.Color.FloralWhite;
-            this.btnEdit.Location = new System.Drawing.Point(858, 582);
+            this.btnEdit.Location = new System.Drawing.Point(572, 378);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(96, 45);
+            this.btnEdit.Size = new System.Drawing.Size(64, 29);
             this.btnEdit.TabIndex = 30;
             this.btnEdit.Text = "EDIT";
             this.btnEdit.UseVisualStyleBackColor = false;
@@ -203,9 +216,10 @@
             this.btnDelete.FlatAppearance.BorderSize = 0;
             this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDelete.ForeColor = System.Drawing.Color.FloralWhite;
-            this.btnDelete.Location = new System.Drawing.Point(1023, 582);
+            this.btnDelete.Location = new System.Drawing.Point(682, 378);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(96, 45);
+            this.btnDelete.Size = new System.Drawing.Size(64, 29);
             this.btnDelete.TabIndex = 31;
             this.btnDelete.Text = "DELETE";
             this.btnDelete.UseVisualStyleBackColor = false;
@@ -214,26 +228,29 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(26, 159);
+            this.label8.Location = new System.Drawing.Point(17, 103);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(153, 22);
+            this.label8.Size = new System.Drawing.Size(107, 14);
             this.label8.TabIndex = 32;
             this.label8.Text = "Find a Category";
             // 
             // txtFind
             // 
             this.txtFind.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.txtFind.Location = new System.Drawing.Point(199, 159);
+            this.txtFind.Location = new System.Drawing.Point(133, 103);
+            this.txtFind.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtFind.Name = "txtFind";
-            this.txtFind.Size = new System.Drawing.Size(237, 26);
+            this.txtFind.Size = new System.Drawing.Size(159, 20);
             this.txtFind.TabIndex = 33;
+            this.txtFind.TextChanged += new System.EventHandler(this.txtFind_TextChanged);
             // 
             // Categories
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FloralWhite;
-            this.ClientSize = new System.Drawing.Size(1164, 744);
+            this.ClientSize = new System.Drawing.Size(776, 484);
             this.Controls.Add(this.txtFind);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.btnDelete);
@@ -249,10 +266,12 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "Categories";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Categories";
+            this.Load += new System.EventHandler(this.Categories_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCategories)).EndInit();
--- AliceMarket/AliceMarket/Categories.cs
+++ AliceMarket/AliceMarket/Categories.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -16,5 +15,124 @@ namespace AliceMarket
         {
             InitializeComponent();
         }
+
+        AliceMarketEntities market = new AliceMarketEntities();
+
+        public void ListCategories()
+        {
+            dgvCategories.DataSource = market.CategoriesTbls.ToList();
+        }
+
+        public void Clear()
+        {
+            txtCategoryID.Text = txtCtgName.Text = tctCtgDesc.Text = "";
+        }
+
+        private void Categories_Load(object sender, EventArgs e)
+        {
+            ListCategories();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Create a new category object and populate it with values from input fields
+            var category = new CategoriesTbl
+            {
+                CategoryName = txtCtgName.Text,
+                Description = tctCtgDesc.Text
+            };
+
+            market.CategoriesTbls.Add(category);
+            market.SaveChanges();
+
+            MessageBox.Show("New category is saved");
+            ListCategories();
+            Clear();
+        }
+
+        private void dgvCategories_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0) // Ensure a valid row is clicked
+            {
+                txtCategoryID.Text = dgvCategories.Rows[e.RowIndex].Cells[0].Value.ToString();
+                txtCtgName.Text = dgvCategories.Rows[e.RowIndex].Cells[1].Value.ToString();
+                tctCtgDesc.Text = dgvCategories.Rows[e.RowIndex].Cells[2].Value.ToString();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtCategoryID.Text, out int categoryId))
+            {
+                var category = market.CategoriesTbls.FirstOrDefault(c => c.CategoryID == categoryId);
+
+                if (category != null)
+                {
+                    market.CategoriesTbls.Remove(category);
+                    market.SaveChanges();
+
+                    MessageBox.Show("Category is deleted.");
+                    ListCategories();
+                    Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Category not found.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a valid category.");
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtCategoryID.Text, out int categoryId))
+            {
+                var category = market.CategoriesTbls.FirstOrDefault(c => c.CategoryID == categoryId);
+
+                if (category != null)
+                {
+                    category.CategoryName = txtCtgName.Text;
+                    category.Description = tctCtgDesc.Text;
+
+                    market.SaveChanges();
+
+                    MessageBox.Show("Update is made successfully");
+                    ListCategories();
+                    Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Category not found.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a valid category.");
+            }
+        }
+
+        private void txtFindCategory_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtFind.Text))
+            {
+                var foundCategories = market.CategoriesTbls
+                    .Where(c => c.CategoryName.Contains(txtFind.Text))
+                    .ToList();
+                dgvCategories.DataSource = foundCategories;
+            }
+            else
+            {
+                ListCategories();
+            }
+        }
+
+        private void txtFind_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
